2a574dcbd2555f3dac5311f70fb5345186c685a5
Ronald Luo <loblivious.ee@gmail.com>
2023-08-12 16:31:18 +0800

    creating a messages service to interact with firestore - 2/2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgModule,
+  OnInit,
+} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { combineLatest, map } from 'rxjs';
+import { HomeStore } from './data-access/home.store';
+import { MessageListComponentModule } from './ui/message-list.component';
 
 @Component({
   selector: 'app-home',
   template: `
-    <ion-header>
-      <ion-toolbar>
-        <ion-title> Home </ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content> </ion-content>
+    <ng-container *ngIf="vm$ | async as vm">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title> Chat </ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <app-message-list [messages]="vm.messages"></app-message-list>
+      </ion-content>
+    </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [HomeStore],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  vm$ = combineLatest([this.store.messages$]).pipe(
+    map(([messages]) => ({ messages }))
+  );
+
+  constructor(private store: HomeStore) {}
+
+  ngOnInit() {
+    this.store.loadMessages();
+  }
+}
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     IonicModule,
+    MessageListComponentModule,
     RouterModule.forChild([
       {
         path: '',
